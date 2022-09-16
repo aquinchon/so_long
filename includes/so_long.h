aabde6ebf55d97a60bbf94345318fe6620ee1727
Arnaud Quinchon <36507528+aquinchon@users.noreply.github.com>
2022-09-16 13:08:52 +0200

    Add files via upload

--- includes/so_long.h
+++ includes/so_long.h
@@ -20,6 +20,7 @@
 # include <fcntl.h>
 # include "libft.h"
 # include <mlx.h>
+//# include <mlx_int.h>
 
 #include <stdio.h>
 
@@ -29,7 +30,7 @@
 # define K_RT	65363
 # define K_DN	65364
 
-# define	TEX_SIZE	32
+# define TEX_SIZE	32
 
 typedef struct s_coord
 {
@@ -75,22 +76,28 @@ typedef struct s_game
 	int		width;
 	int		height;
 	t_pmx	wall;
-	t_pmx	sand;
+	t_pmx	ground;
+	t_pmx	player;
+	t_pmx	item;
+	t_pmx	exit;
 	int		move_cnt;
 	int		item_collect;
-	int		pos;
+	int		player_pos;
 }	t_game;
 
 /* Errors management ft_errors.c */
-int ft_error_arg(char *msg);
-int	ft_errors_free(char *msg, t_game *game);
-int	ft_errors(char *msg);
+int		ft_error_arg(char *msg);
+int		ft_errors_free(char *msg, t_game *game);
+int		ft_errors(char *msg);
 /* Free management ft_free.c*/
 void	ft_free_map(t_map *map);
+void	ft_free_mlx(t_game *game);
 void	ft_free_game(t_game *game);
 /* Initialization ft_init.c */
 t_map	*ft_initialize_map(void);
 t_game	*ft_initialize(void);
+/* Initialization mlx ft_init_mlx.c */
+int		ft_initialize_mlx(t_game *game);
 /* Map reading ft_map.c*/
 int		ft_read_map(char *filename, t_map *map);
 /* Map checking ft_check_map.c*/
@@ -98,7 +105,7 @@ int		ft_check_map(t_map *map);
 /* Draw utilities ft_draw_utils.c*/
 void	ft_get_draw_size(t_map *map);
 /* play game ft_game.c */
-int	ft_game(t_game *game);
+int		ft_game(t_game *game);
 
 
 typedef struct s_vars
