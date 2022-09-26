28459c0e696217b470814f0535410e8aca27dd74
Arnaud Quinchon <36507528+aquinchon@users.noreply.github.com>
2022-09-26 10:58:31 +0200

    Add files via upload

--- srcs/ft_draw_map.c
+++ srcs/ft_draw_map.c
@@ -14,21 +14,9 @@
 
 static void	ft_draw_items(t_game *game)
 {
-	int	i;
-
-	i = -1;
-	while (++i < game->map->width * game->map->height)
-	{
-		if (game->map->map[i] == 'C')
-			mlx_put_image_to_window(game->mlx, game->win, game->item.img,
-				(i % game->map->width) * 32, (i / game->map->width) * 32);
-		else if (i == game->player_pos)
-			mlx_put_image_to_window(game->mlx, game->win, game->player.img,
-				(i % game->map->width) * 32, (i / game->map->width) * 32);
-		else if (game->map->map[i] == 'E')
-			mlx_put_image_to_window(game->mlx, game->win, game->exit.img,
-				(i % game->map->width) * 32, (i / game->map->width) * 32);
-	}
+	mlx_put_image_to_window(game->mlx, game->win, game->player.img,
+		(game->player_pos % game->map->width) * 32,
+		(game->player_pos / game->map->width) * 32);
 }
 
 void	ft_draw_map(t_game *game)
@@ -40,6 +28,12 @@ void	ft_draw_map(t_game *game)
 	{
 		if (game->map->map[i] == '1')
 			mlx_put_image_to_window(game->mlx, game->win, game->wall.img,
+				(i % game->map->width) * 32, (i / game->map->width) * 32);
+		else if (game->map->map[i] == 'C')
+			mlx_put_image_to_window(game->mlx, game->win, game->item.img,
+				(i % game->map->width) * 32, (i / game->map->width) * 32);
+		else if (game->map->map[i] == 'E')
+			mlx_put_image_to_window(game->mlx, game->win, game->exit.img,
 				(i % game->map->width) * 32, (i / game->map->width) * 32);
 		else
 			mlx_put_image_to_window(game->mlx, game->win, game->ground.img,
